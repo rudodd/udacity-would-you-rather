c35a43c55d60d165eab8aa5660227e591d5a0c61
Rustin Dodd <rustindodd@gmail.com>
2022-02-04 16:11:19 -0600

    initial redux setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,7 @@
 // Import libraries
 import react from 'react';
-import { Link, Route, Routes} from 'react-router-dom';
-import * as Redux from 'redux';
-import * as ReduxThunk from 'redux-thunk';
-import * as Data from '../_DATA.js';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
-// Import actions
-import initialDataHandler from '../actions/data';
-
-// Import reducers
-import loading from '../reducers/loading';
 
 // Import Components, etc...
 import '../dist/css/app.css';
@@ -18,47 +10,31 @@ import Question from './question';
 import Leaders from './leaders';
 import Header from './header';
 
-// Actions
-
-
-// Reducers
-
-
-// Middleware
-const logger = (store) => (next) => (action) => {
-  console.group(action.type)
-    console.log('The action: ', action)
-    const result = next(action)
-    console.log('The new state: ', store.getState())
-  console.groupEnd()
-  return result
-}
-
 class App extends react.Component {
 
   componentDidMount () {
-    const store = Redux.createStore(Redux.combineReducers({
-      loading,
-    }), Redux.applyMiddleware(ReduxThunk.default, logger));
-    store.dispatch(initialDataHandler());
-    store.subscribe(() => this.forceUpdate());
+    
   }
 
   render() {
 
-    if (loading === false) {
-      console.log(this.store);
+    if (this.props.loading) {
+      return (
+        <h1>Loading...</h1>
+      )
     }
 
     return (
-      <div className="app-wrapper">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route exact path="/question" element={<Question />} />
-          <Route exact path="/leaders" element={<Leaders />} />
-        </Routes>
-      </div>
+      <BrowserRouter>
+        <div className="app-wrapper">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route exact path="/question" element={<Question />} />
+            <Route exact path="/leaders" element={<Leaders />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import * as Redux from 'redux';
+import { Provider, connect } from 'react-redux';
+import * as ReduxThunk from 'redux-thunk';
+// import * as Data from './_DATA.js';
+
+// Import actions
+import initialDataHandler from './actions/data';
+
+// Import reducers
+import loading from './reducers/loading';
+
+// Import components
 import App from './components/app';
+
+// Middleware
+const logger = (store) => (next) => (action) => {
+  console.group(action.type)
+    console.log('The action: ', action)
+    const result = next(action)
+    console.log('The new state: ', store.getState())
+  console.groupEnd()
+  return result
+}
+
+const store = Redux.createStore(Redux.combineReducers({
+  loading,
+}), Redux.applyMiddleware(ReduxThunk.default, logger));
+store.dispatch(initialDataHandler());
+
+const ConnectedApp = connect((state) => ({
+  loading: state.loading
+}))(App)
 
 ReactDOM.render(
 
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <ConnectedApp />
+  </Provider>,
   document.getElementById('root')
 );
